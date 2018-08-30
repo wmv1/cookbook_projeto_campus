115c1180f702c4dde6821a1efa9e12023bcbde09
Henrique Morato <henrique.morato@campuscode.com.br>
2018-08-30 10:44:03 -0300

    Visitante visualiza detalhes de uma receita

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 3.7'
   gem 'capybara'
+  gem 'shoulda-matchers'
 end
 
 group :development do
@@ -54,6 +55,9 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem 'shoulda-matchers'
+end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_222434) do
+ActiveRecord::Schema.define(version: 2018_08_29_225355) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_08_29_222434) do
     t.integer "cook_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ingredients"
+    t.text "cook_method"
   end
 
 end
